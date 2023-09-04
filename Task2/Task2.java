43b0ba246a03e555d9fc60a1c34efbe5e070d221
mkylb <kylbasovych@gmail.com>
2023-09-04 17:48:52 +0300

    Your commit message here

--- Task2/Task2.java
+++ Task2/Task2.java
@@ -1,6 +1,8 @@
+package Task2;
+
 import java.util.Scanner;
 
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
         Scanner User_Input = new Scanner(System.in);
 
--- Task3/Task3.java
+++ Task3/Task3.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Task3 {
+import java.util.Scanner;
+
+
+public class Task3 {
+    public static void main(String[] args)
+
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double smaller = (num1 < num2) ? num1 : num2;
+        double bigger = (num1 > num2) ? num1 : num2;
+
+        // Ascending order
+        System.out.println("Ascending Order: " + smaller + " " + bigger);
+
+        // Descending order
+        System.out.println("Descending Order: " + bigger + " " + smaller);
+
+        scanner.close();
+    }
 }
