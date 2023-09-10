6849830f91982d20430b1ef87046725e08cd750a
mkylb <kylbasovych@gmail.com>
2023-09-10 16:16:32 +0300

    tasks 14,16,18

--- Homework_Swich_Loop/Task2_14.java
+++ Homework_Swich_Loop/Task2_14.java
@@ -1,2 +1,14 @@
-package Homework_Swich_Loop;public class Task2_14 {
+package Homework_Swich_Loop;
+
+public class Task2_14 {
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int f = 1; f <= n; f++) {
+            for (int j = 1; j <= n; j++) {
+                int multiple = f * j;
+                System.out.println(f + " * " + j + " = " + multiple);
+            }
+        }
+    }
 }
